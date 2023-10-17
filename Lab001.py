68ec915e2af006d582c1f0aee3a326997efb92b0
vasanthpannir <57087413+vasanthpannir@users.noreply.github.com>
2023-10-17 07:28:19 +0530

    Add files via upload

--- Lab001.py
+++ Lab001.py
@@ -1,1 +1,7 @@
-Print("Hello world")
+num1 = int(input("Enter the number :\n"))
+num2 = int(input("Enter the number :\n"))
+num3 = int(input("Enter the number :\n"))
+
+result = num1 if (num1 > num2 and num1 > num3) else (num2 if num2 > num3 else num3)
+print(f"The maximum of {num1},{num2},{num3} is : {result}")
+
